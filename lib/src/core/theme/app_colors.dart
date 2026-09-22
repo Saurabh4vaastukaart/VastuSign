87d19dc10550428303f085e02abd3e1a76bd99be
Saurabh4vaastukaart <saurabhkaushick@gmail.com>
2026-09-22 21:18:53 +0530

    Add lib/src/core/theme/app_colors.dart

---
+++ lib/src/core/theme/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const midnight = Color(0xFF071A2A);
+  static const deepNavy = Color(0xFF0B2639);
+  static const navy = Color(0xFF10364D);
+  static const blue = Color(0xFF1D6A8A);
+  static const ivory = Color(0xFFF9F5EB);
+  static const warmWhite = Color(0xFFFFFCF6);
+  static const sand = Color(0xFFE9DEC8);
+  static const gold = Color(0xFFD2A84D);
+  static const lightGold = Color(0xFFF1D382);
+  static const emerald = Color(0xFF2FA77C);
+  static const amber = Color(0xFFF3A43B);
+  static const coral = Color(0xFFE75E59);
+  static const sky = Color(0xFF73C6D9);
+  static const ink = Color(0xFF10212C);
+  static const muted = Color(0xFF60717B);
+  static const outline = Color(0xFFD9D4C9);
+}
+
